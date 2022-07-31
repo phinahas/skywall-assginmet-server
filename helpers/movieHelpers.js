c496db2a8a8649075937adf8f3d51044e02c009c
Phinahas Philip <phinahasphilip2000@gmail.com>
2022-07-31 12:00:26 +0530

    Api for movie with rating 8.0 and movies released after 2010 made

--- helpers/movieHelpers.js
+++ helpers/movieHelpers.js
@@ -1,18 +1,51 @@
-const db = require('../config/connection');
-const collection = require('../config/collections');
-const objectId = require('mongodb').ObjectID;
+const db = require("../config/connection");
+const collection = require("../config/collections");
+const objectId = require("mongodb").ObjectID;
 
+module.exports = {
+  createMovie: (movie) => {
+    return new Promise((resolve, reject) => {
+      db.get()
+        .collection(collection.MOVIES)
+        .insertMany(movie)
+        .then(() => {
+          resolve();
+        });
+    });
+  },
 
-module.exports={
+  getMovies2010: () => {
+    return new Promise(async (resolve, reject) => {
+      let movies = await db
+        .get()
+        .collection(collection.MOVIES)
+        .find({ yearOfRelease: { $gte: 2010 } })
+        .toArray();
 
-    createMovie:(movie) => {
+      await movies.sort((a, b) => {
+        let ma = a.movieName.toLowerCase();
+        let mb = b.movieName.toLowerCase();
 
-        return new Promise((resolve, reject) => {
-            db.get().collection(collection.MOVIES).insertOne(movie).then(()=>{
-                resolve();
-            })
-        })
+        if (ma < mb) {
+          return -1;
+        }
+        if (ma > mb) {
+          return 1;
+        }
+        return 0;
+      });
 
-    }
+      resolve(movies);
+    });
+  },
 
-}
+  getMoviesRating : ()=>{
+    return new Promise(async (resolve, reject) =>{
+
+      let movies = await db.get().collection(collection.MOVIES).find({rating:{$gte:8.0}}).toArray();
+      //console.log(movies);
+      resolve(movies);
+
+    })
+  }
+};
--- routes/queriesRoutes.js
+++ routes/queriesRoutes.js
@@ -3,9 +3,31 @@ const router = express.Router();
 const movieHelpers = require('../helpers/movieHelpers')
 
 router.post('/add-movies',(req,res)=>{
-    console.log(req.body);
+    //console.log(req.body);
+    // let movieSchema = {
+    //     movieName:"",
+    //     movieDirector:"",
+    //     yearOfRelease:null,
+    //     rating:null,
+    //     cast:[]
+    // }
     movieHelpers.createMovie(req.body).then((response)=>{
         res.json({success:"Added"})
+    })
+
+})
+
+router.get('/movies-2010',(req,res)=>{
+    movieHelpers.getMovies2010().then((response)=>{
+        res.status(200).json({success:"true",data:response})
+
+    })
+   
+})
+
+router.get('/movies-rating-8.0',(req,res)=>{
+    movieHelpers.getMoviesRating().then((response)=>{
+        res.status(200).json({success:"true",data:response})
     })
 
 })
